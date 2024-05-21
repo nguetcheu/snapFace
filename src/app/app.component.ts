1dafaf149da779c76a31ec1719dfbdbe18c5745e
nguetcheu <dnguetcheu@gmail.com>
2024-05-21 13:45:18 +0200

    Gestion *ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,37 @@ import { FaceSnap } from './models/face-snap.model';
 })
 export class AppComponent implements OnInit {
   mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
 
   title = 'snap';
 
   ngOnInit() {
-    this.mySnap = new FaceSnap(
-      'Archibald',
-      'Mon meilleur ami depuis tout petit !',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      0
-    );
+    this.mySnap = {
+      title: 'Archibald',
+      description: 'Mon meilleur ami depuis tout petit !',
+      imageUrl:
+        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+      location: 'Paris',
+    };
+    this.myOtherSnap = {
+      title: 'Three Rock Mountain',
+      description: 'Un endroit magnifique pour les randonnées.',
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+      location: 'Dublin',
+    };
+    this.myLastSnap = {
+      title: 'Un bon repas',
+      description: "Mmmh que c'est bon !",
+      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+      location: 'Miami',
+    };
   }
 }
