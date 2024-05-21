d227140b37cc126bf7ea80b0e3858ed7eec866fb
nguetcheu <dnguetcheu@gmail.com>
2024-05-21 11:21:24 +0200

    Gestion des propriétes d'un component

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
-  styleUrls: ['./face-snap.component.scss']
+  styleUrls: ['./face-snap.component.scss'],
 })
 export class FaceSnapComponent implements OnInit {
+  title!: string;
+  desscription!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.title = 'Archibald';
+    this.desscription = 'Mon meilleur ami depuis tout petit';
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.imageUrl =
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
   }
-
 }
