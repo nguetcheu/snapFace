065129d907a1bf0bf7967db41fcb4d8e1b5b801f
nguetcheu <dnguetcheu@gmail.com>
2024-05-22 15:11:09 +0200

    Restructuration de app et début crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, FaceSnapComponent, FaceSnapListComponent],
+  declarations: [AppComponent, FaceSnapComponent, FaceSnapListComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -1,9 +1,11 @@
 export class FaceSnap {
-  constructor(public title: string,
-              public description: string,
-              public imageUrl: string,
-              public createdDate: Date,
-              public snaps: number,
-              public location?: string) {
-  }
+  constructor(
+    public id: number,
+    public title: string,
+    public description: string,
+    public imageUrl: string,
+    public createdDate: Date,
+    public snaps: number,
+    public location?: string
+  ) {}
 }
