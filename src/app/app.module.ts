fd23de89ab1918d7423da8a911b80fab9a375f3c
nguetcheu <dnguetcheu@gmail.com>
2024-05-22 14:13:50 +0200

    Restructuration de app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 
 @NgModule({
-  declarations: [AppComponent, FaceSnapComponent],
+  declarations: [AppComponent, FaceSnapComponent, FaceSnapListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
